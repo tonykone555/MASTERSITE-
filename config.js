da3d3087978ae4530077f94a6d562e8dabf8da28
tonykone555 <tonykone555@gmail.com>
2026-09-24 10:18:05 +0200

    Load natural 3D slab realism layer

--- config.js
+++ config.js
@@ -16,11 +16,16 @@ document.head.appendChild(planSheet);
 
 const surfaceSheet=document.createElement('link');
 surfaceSheet.rel='stylesheet';
-surfaceSheet.href='surface-sections.css?v=floating-slabs-1';
+surfaceSheet.href='surface-sections.css?v=floating-slabs-2';
 document.head.appendChild(surfaceSheet);
+
+const realismSheet=document.createElement('link');
+realismSheet.rel='stylesheet';
+realismSheet.href='slab-realism.css?v=natural-edges-1';
+document.head.appendChild(realismSheet);
 
 setTimeout(()=>{
   const surfaceScript=document.createElement('script');
-  surfaceScript.src='surface-sections.js?v=floating-slabs-1';
+  surfaceScript.src='surface-sections.js?v=floating-slabs-2';
   document.body.appendChild(surfaceScript);
 },0);
